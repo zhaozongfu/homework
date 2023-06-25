b73affad0301fd8a9039c7d49c8686ace67f0b14
江上云起 <131733143+zhaozongfu@users.noreply.github.com>
2023-06-25 21:50:18 +0800

    Add files via upload

---
+++ 6.25考试(赵宗福)/js/壁纸管理.js
@@ -0,0 +1,96 @@
+$(document).ready(function () {
+    // 点击图标
+    $('#icon').click(function () {
+        // 占满整个屏幕
+        $('#box').toggleClass('show');
+    });
+    $("#span").on("click", function () {
+
+        $('body').toggleClass('fff');
+    })
+    $("#span1").on("click", function () {
+
+        $('body').toggleClass('fff1');
+    })
+    $("#span2").on("click", function () {
+
+        $('body').toggleClass('fff2');
+    })
+    $("#span3").on("click", function () {
+
+        $('body').toggleClass('fff3');
+    })
+    $("#span4").on("click", function () {
+
+        $('body').toggleClass('fff4');
+    })
+    $("#span5").on("click", function () {
+        $('body').toggleClass('fff5');
+    })
+    $("#span6").on("click", function () {
+
+        $('body').toggleClass('fff6');
+    })
+    $("#span7").on("click", function () {
+
+        $('body').toggleClass('fff7');
+    })
+    $("#span8").on("click", function () {
+
+        $('body').toggleClass('fff8');
+    })
+    $("#span9").on("click", function () {
+        $('body').toggleClass('fff9');
+    })
+    $("#span10").on("click", function () {
+
+        $('body').toggleClass('fff10');
+    })
+    $("#span11").on("click", function () {
+
+        $('body').toggleClass('fff11');
+    })
+    $("#span12").on("click", function () {
+
+        $('body').toggleClass('fff12');
+    })
+});
+/* const colorli = [
+    {
+        color: "red"
+    },
+    {
+        color1: "pink"
+    },
+    {
+        color2: "blue"
+    },
+    {
+        color3: "yellow"
+    },
+    {
+        color4: "green"
+    },
+    {
+        color5: "aqua"
+    }
+];
+$(function(){
+    styleCss();
+});
+function styleCss(){
+    let html="";
+    colorli.forEach((item,index)=>{
+        html +=
+        `
+        <div style="background-color:${item.color};"></div>
+        <div style="background-color:${item.color1};"></div>
+        <div style="background-color:${item.color2};"></div>
+        <div style="background-color:${item.color3};"></div>
+        <div style="background-color:${item.color4};"></div>
+        <div style="background-color:${item.color5};"></div>
+        `
+    });
+    $("#box3").html(html);
+} */
+
